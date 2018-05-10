779ce52c1965ec6589d721719064b1226463f391
samreid <reids@colorado.edu>
2018-05-09 19:17:40 -0600

    Moved generateDocumentation.js, see #1

--- js/generateDocumentation.js
+++ js/generateDocumentation.js
@@ -1,50 +1,60 @@
-// const puppeteer = require( 'puppeteer' );
-//
-// ( async () => {
-//   const browser = await puppeteer.launch();
-//   const page = await browser.newPage();
-//
-//   // TODO: get the list of sims
-//
-//   // TODO: add a query parameter that directs it to include the screen captures
-//   await page.goto( 'http://localhost/acid-base-solutions/acid-base-solutions_en.html?brand=phet&ea&postMessageOnLoad' );
-//
-//   // Get the "viewport" of the page, as reported by the page.
-//   const dimensions = await page.evaluate( () => {
-//     return {
-//       width: document.documentElement.clientWidth,
-//       height: document.documentElement.clientHeight,
-//       deviceScaleFactor: window.devicePixelRatio,
-//       phet: phet
-//     };
-//   } );
-//
-//   console.log( 'Dimensions:', dimensions );
-//
-//   await browser.close();
-// } )();
-
 'use strict';
 
 const puppeteer = require( 'puppeteer' );
+const fs = require( 'fs' );
+
+// Helper function to get the sim list from perennial
+const getSims = function() {
+  const simsFileText = fs.readFileSync( '../perennial/data/active-sims' ).toString();
+  const sims = simsFileText.replace( new RegExp( '\r', 'g' ), '' ).split( '\n' );
+  sims.splice( sims.indexOf( '' ) );
+  return sims;
+};
 
 ( async () => {
   const browser = await puppeteer.launch();
-  const page = await browser.newPage();
 
-  // Define a window.onCustomEvent function on the page.
-  // await page.exposeFunction('onCustomEvent', e => {
-  //   console.log(`${e.type} fired`, e.detail || '');
-  // });
+  let data = {};
 
-  await page.evaluateOnNewDocument( function() {
-    console.log( 'hello 1' );
-    window.addEventListener( 'message', function( a ) {
-      console.log( 'hello from a' );
+  let sims = getSims();
+
+  async function loadSim( i ) {
+
+    const sim = sims[ i ];
+
+    const page = await browser.newPage();
+
+    // log to our server from the browser
+    page.on( 'console', msg => console.log( 'PAGE LOG:', msg.text() ) );
+
+    console.log( 'loading: ' + sim );
+
+    // navigate to the sim page
+    await page.goto( `http://localhost/${sim}/${sim}_en.html?brand=phet&ea&postMessageOnLoad` );
+
+    // This will not return until the first message has been sent
+    // TODO in our sims, the first message is the loaded one, but we should parse the JSON and make sure that it is the laoded event.
+    data[ sim ] = await page.evaluate( () => {
+      return new Promise( function( resolve, reject ) {
+        window.addEventListener( 'message', function() {
+
+          // TODO: SR!!! resolve with whatever we want to be stored for this sim.
+          resolve( 'from the browser: ' + document.title );
+        } );
+        // TODO: reject?
+      } );
     } );
-  } );
 
-  await page.goto( 'http://localhost/acid-base-solutions/acid-base-solutions_en.html?brand=phet&ea&postMessageOnLoad', { waitUntil: 'networkidle2' } );
+    console.log( data ); // current progress
+    await page.close();
+    if ( i + 1 < sims.length ) {
+      await loadSim( i + 1 );
+    }
+  }
 
-  // await browser.close();
+  // start loading the first sim.
+  await loadSim( 0 );
+
+  console.log( `data: `, JSON.stringify( data, null, 2 ) );
+  await browser.close();
 } )();
