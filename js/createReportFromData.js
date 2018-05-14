d87a70e7c9b66ff4fa54ff89fc2dfa3d59c3cfd7
zepumph <michael.kauzmann@colorado.edu>
2018-05-14 12:24:56 -0800

    no need of iife in nodejs, added image readme, https://github.com/phetsims/binder/issues/7

--- js/createReportFromData.js
+++ js/createReportFromData.js
@@ -2,32 +2,33 @@
 
 /**
  * Combine JSON and MD into an HTML report.
+ * @author Sam Reid (PhET Interactive Simulations)
  */
-( function() {
 
-  'use strict';
+/* eslint-env node */
+'use strict';
 
-  const fs = require( 'fs' );
-  const marked = require( 'marked' );
+const fs = require( 'fs' );
+const marked = require( 'marked' );
 
-  /**
-   * @param {Object} data sims=>components=>dataURLs
-   * @returns {string}
-   */
-  const createReportFromData = function( data ) {
+/**
+ * @param {Object} data sims=>components=>dataURLs
+ * @returns {string}
+ */
+const createReportFromData = function( data ) {
 
-    const componentTypes = {};
-    const sims = Object.keys( data );
-    sims.forEach( sim => {
-      const componentTypesForSim = Object.keys( data[ sim ] );
-      componentTypesForSim.forEach( componentTypeForSim => {
-        componentTypes[ componentTypeForSim ] = null; // Just enumerating keys
-      } );
+  const componentTypes = {};
+  const sims = Object.keys( data );
+  sims.forEach( sim => {
+    const componentTypesForSim = Object.keys( data[ sim ] );
+    componentTypesForSim.forEach( componentTypeForSim => {
+      componentTypes[ componentTypeForSim ] = null; // Just enumerating keys
     } );
+  } );
 
-    const componentTypesArray = Object.keys( componentTypes );
+  const componentTypesArray = Object.keys( componentTypes );
 
-    let header = `<html><head>
+  let header = `<html><head>
 <style>
  body {
     background-color: #acd7ed;
@@ -41,64 +42,63 @@
 </style>
 </head>
 <body>`;
-    componentTypesArray.forEach( function( repoAndComponent ) {
+  componentTypesArray.forEach( function( repoAndComponent ) {
 
-      let repo = repoAndComponent.split( '/' )[ 0 ];
-      let component = repoAndComponent.split( '/' )[ 1 ];
+    let repo = repoAndComponent.split( '/' )[ 0 ];
+    let component = repoAndComponent.split( '/' )[ 1 ];
 
-      const simsThatUseTheComponent = sims.filter( sim => {
-        return data[ sim ][ repoAndComponent ];
-      } );
-      const numberOfSimsThatUseTheComponent = simsThatUseTheComponent.length;
+    const simsThatUseTheComponent = sims.filter( sim => {
+      return data[ sim ][ repoAndComponent ];
+    } );
+    const numberOfSimsThatUseTheComponent = simsThatUseTheComponent.length;
 
-      let markdown = '';
-      try {
-        const m = fs.readFileSync( `../../${repo}/docs/${component}.md` );
-        markdown = marked( m.toString() );
+    let markdown = '';
+    try {
+      const m = fs.readFileSync( `../../${repo}/docs/${component}.md` );
+      markdown = marked( m.toString() );
 
-        // Use subdirectory for images, so that different directories can have images of the same name
-        // TODO: This may yield false positives, say if code examples have this same term.
-        markdown = markdown.split( '<img src="images/' ).join( '<img src="images/' + repo + '/' );
-      }
-      catch( e ) {
-        markdown = marked( '# TODO: *documentation*' );
-      }
+      // Use subdirectory for images, so that different directories can have images of the same name
+      // TODO: This may yield false positives, say if code examples have this same term.
+      markdown = markdown.split( '<img src="images/' ).join( '<img src="images/' + repo + '/' );
+    }
+    catch( e ) {
+      markdown = marked( '# TODO: *documentation*' );
+    }
 
-      // TODO: support subdirectories for the component.
-      header = header + `<h1>${repoAndComponent}</h1>
+    // TODO: support subdirectories for the component.
+    header = header + `<h1>${repoAndComponent}</h1>
 <ul>
 <li><a href="https://github.com/phetsims/${repo}/blob/master/js/${component}.js">Source Code and Options</a></li>
 <li>Number of published sims with component:</li>
 <li>Number of development sims with component: ${numberOfSimsThatUseTheComponent}</li>
 </ul>` + markdown;
-      const simReports = sims.map( sim => {
-        const components = data[ sim ][ repoAndComponent ];
-        if ( components ) {
-          return '<section>' + sim + ':' + ( components.map( c => {
-            return '<image src=' + c + '></image>';
-          } ).join( ' ' ) ) + '</section>';
-        }
-        else {
-          return '<section>' + sim + ':' + 'not used</section>';
-        }
-      } );
-      header = header + simReports.join( '\n' );
+    const simReports = sims.map( sim => {
+      const components = data[ sim ][ repoAndComponent ];
+      if ( components ) {
+        return '<section>' + sim + ':' + ( components.map( c => {
+          return '<image src=' + c + '></image>';
+        } ).join( ' ' ) ) + '</section>';
+      }
+      else {
+        return '<section>' + sim + ':' + 'not used</section>';
+      }
     } );
-    header = header + '</body></html>';
+    header = header + simReports.join( '\n' );
+  } );
+  header = header + '</body></html>';
 
-    return header;
-  };
-  /**
-   * @param data sim => componentName => [dataURLs]
-   * @returns {string}
-   */
-  module.exports = createReportFromData;
+  return header;
+};
+/**
+ * @param data sim => componentName => [dataURLs]
+ * @returns {string}
+ */
+module.exports = createReportFromData;
 
 // Shortcut to use stored JSON for quick iteration.
-  const myArgs = process.argv.slice( 2 );
-  if ( myArgs[ 0 ] && myArgs[ 0 ] === 'json' ) {
-    const inputFile = myArgs[ 1 ];
-    const report = createReportFromData( JSON.parse( fs.readFileSync( inputFile ) ) );
-    console.log( report );
-  }
-} )();
+const myArgs = process.argv.slice( 2 );
+if ( myArgs[ 0 ] && myArgs[ 0 ] === 'json' ) {
+  const inputFile = myArgs[ 1 ];
+  const report = createReportFromData( JSON.parse( fs.readFileSync( inputFile ) ) );
+  console.log( report );
+}
--- js/generateDocumentation.js
+++ js/generateDocumentation.js
@@ -6,110 +6,110 @@
  * @author Sam Reid (PhET Interactive Simulations)
  * @author Michael Kauzmann (PhET Interactive Simulations)
  */
-( function() {
-  'use strict';
 
-  // modules
-  const puppeteer = require( 'puppeteer' );
-  const fs = require( 'fs' );
-  const fsExtra = require( 'fs-extra' ); // eslint-disable-line
-  const createReportFromData = require( './createReportFromData' );
+/* eslint-env node */
+'use strict';
 
-  // constants
-  const OUTPUT_FILE = '../docs/index.html';
+// modules
+const puppeteer = require( 'puppeteer' );
+const fs = require( 'fs' );
+const fsExtra = require( 'fs-extra' ); // eslint-disable-line
+const createReportFromData = require( './createReportFromData' );
 
-  // Helper function to get the sim list from perennial
-  const getSims = function() {
-    return fs.readFileSync( '../../perennial/data/active-sims' ).toString().trim().split( '\n' ).map( sim => sim.trim() );
-  };
+// constants
+const OUTPUT_FILE = '../docs/index.html';
 
-  const myArgs = process.argv.slice( 2 );
+// Helper function to get the sim list from perennial
+const getSims = function() {
+  return fs.readFileSync( '../../perennial/data/active-sims' ).toString().trim().split( '\n' ).map( sim => sim.trim() );
+};
 
-  const commandLineSims = myArgs[ 0 ]; // Allow comma-separated list of sims
-  if ( !OUTPUT_FILE ) {
-    throw new Error( 'Usage: specify outputFile' );
-  }
-  console.log( 'streaming to ' + OUTPUT_FILE );
+const myArgs = process.argv.slice( 2 );
 
-  ( async () => {
-    const browser = await puppeteer.launch();
+const commandLineSims = myArgs[ 0 ]; // Allow comma-separated list of sims
+if ( !OUTPUT_FILE ) {
+  throw new Error( 'Usage: specify outputFile' );
+}
+console.log( 'streaming to ' + OUTPUT_FILE );
 
-    let data = {};
+( async () => {
+  const browser = await puppeteer.launch();
 
-    let sims = commandLineSims ? commandLineSims.split( ',' ) : getSims();
-    console.log( 'sims to load:', sims.join( ', ' ) );
+  let data = {};
 
-    for ( let sim of sims ) {
+  let sims = commandLineSims ? commandLineSims.split( ',' ) : getSims();
+  console.log( 'sims to load:', sims.join( ', ' ) );
 
-      const page = await browser.newPage();
+  for ( let sim of sims ) {
 
-      // log to our server from the browser
-      page.on( 'console', msg => {
-        if ( msg.type() === 'error' ) {
-          console.error( `${sim} PAGE ERROR:`, msg.text() );
-        }
-        else {
-          console.log( `${sim} PAGE LOG:`, msg.text() );
-        }
-      } );
+    const page = await browser.newPage();
 
-      // navigate to the sim page
-      const url = `http://localhost/${sim}/${sim}_en.html?brand=phet&ea&postMessageOnLoad&binder`;
-      console.log( '\nloading: ' + sim );
-      await page.goto( url );
+    // log to our server from the browser
+    page.on( 'console', msg => {
+      if ( msg.type() === 'error' ) {
+        console.error( `${sim} PAGE ERROR:`, msg.text() );
+      }
+      else {
+        console.log( `${sim} PAGE LOG:`, msg.text() );
+      }
+    } );
 
-      // Add a listener such that when the sim posts a message saying that it has loaded,
-      // get the InstanceRegistry's mapping of components for this sim
-      data[ sim ] = await page.evaluate( ( sim ) => {
-        return new Promise( function( resolve, reject ) {
-          window.addEventListener( 'message', function( event ) {
-            if ( event.data ) {
-              try {
-                let messageData = JSON.parse( event.data );
-                if ( messageData.type === 'load' ) {
-                  console.log( 'loaded', sim );
+    // navigate to the sim page
+    const url = `http://localhost/${sim}/${sim}_en.html?brand=phet&ea&postMessageOnLoad&binder`;
+    console.log( '\nloading: ' + sim );
+    await page.goto( url );
 
-                  if ( phet.phetCore.InstanceRegistry ) {
-                    resolve( phet.phetCore.InstanceRegistry.map );
-                  }
-                  else {
-                    console.error( 'InstanceRegistry not defined. This normally means no components are in this sim.' );
-                    resolve( {} );
-                  }
+    // Add a listener such that when the sim posts a message saying that it has loaded,
+    // get the InstanceRegistry's mapping of components for this sim
+    data[ sim ] = await page.evaluate( ( sim ) => {
+      return new Promise( function( resolve, reject ) {
+        window.addEventListener( 'message', function( event ) {
+          if ( event.data ) {
+            try {
+              let messageData = JSON.parse( event.data );
+              if ( messageData.type === 'load' ) {
+                console.log( 'loaded', sim );
+
+                if ( phet.phetCore.InstanceRegistry ) {
+                  resolve( phet.phetCore.InstanceRegistry.map );
+                }
+                else {
+                  console.error( 'InstanceRegistry not defined. This normally means no components are in this sim.' );
+                  resolve( {} );
                 }
               }
-              catch( e ) {
-                // message isn't what we wanted it to be, so ignore it
-              }
             }
-            else {
-              console.log( 'no data on message event' );
+            catch( e ) {
+              // message isn't what we wanted it to be, so ignore it
             }
-          } );
+          }
+          else {
+            console.log( 'no data on message event' );
+          }
         } );
-      }, sim );
-      await page.close();
-      const report = createReportFromData( data );
-      fs.writeFileSync( OUTPUT_FILE, report );
+      } );
+    }, sim );
+    await page.close();
+    const report = createReportFromData( data );
+    fs.writeFileSync( OUTPUT_FILE, report );
 
-      // Copy image files
-      try {
+    // Copy image files
+    try {
 
-        // TODO: this assumes we only need image from two repos
-        fsExtra.copySync( '../../sun/docs/images', '../docs/images/sun' );
-        fsExtra.copySync( '../../scenery-phet/docs/images', '../docs/images/scenery-phet' );
-      }
-      catch( err ) {
-        console.error( err );
-      }
-
-      console.log( `wrote report to: ${OUTPUT_FILE}` );
+      // TODO: this assumes we only need image from two repos
+      fsExtra.copySync( '../../sun/docs/images', '../docs/images/sun' );
+      fsExtra.copySync( '../../scenery-phet/docs/images', '../docs/images/scenery-phet' );
+    }
+    catch( err ) {
+      console.error( err );
     }
 
-    browser.close();
-    const reportString = createReportFromData( data );
-    fs.writeFileSync( 'binderjson.json', JSON.stringify( data, null, 2 ) );
-    fs.writeFileSync( OUTPUT_FILE, reportString );
-    console.log( `wrote final report to:  ${OUTPUT_FILE}` );
-  } )();
+    console.log( `wrote report to: ${OUTPUT_FILE}` );
+  }
+
+  browser.close();
+  const reportString = createReportFromData( data );
+  fs.writeFileSync( 'binderjson.json', JSON.stringify( data, null, 2 ) );
+  fs.writeFileSync( OUTPUT_FILE, reportString );
+  console.log( `wrote final report to:  ${OUTPUT_FILE}` );
 } )();
