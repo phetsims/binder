2b7f7f32e29719cde0917365ddf237ff23951fb2
chrisklus <christopher.klusendorf@colorado.edu>
2019-07-23 00:17:58 -0600

    Add initial nav menu, see https://github.com/phetsims/binder/issues/26

--- react-binder/src/pages/index/index.js
+++ react-binder/src/pages/index/index.js
@@ -14,14 +14,40 @@ export default class IndexPage extends React.Component {
   constructor( props ) {
     super( props );
 
-    this.state = {};
+    this.state = {
+      currentPage: <h1></h1>,
+      simsPage: <h1>sims page</h1>,
+      componentsPage: <h1>components page</h1>
+    };
+  }
+
+  loadPage( pageToLoad ) {
+    this.setState( {
+      currentPage: pageToLoad
+    } );
   }
 
   render() {
 
     return (
       <div id='index-page'>
-        <h1>Hello World</h1>
+        <div id='side-nav'>
+          <div className='title-container'>
+            <img src='/img/phet.png' className='title-image' alt='PhET'/>
+            <h1 className='title-text'>Binder</h1>
+          </div>
+          <div className='nav-buttons'>
+            <button className='nav-button' onClick={() => this.loadPage( this.state.simsPage )}>
+              SIMS BY COMPONENT
+            </button>
+            <button className='nav-button' onClick={() => this.loadPage( this.state.componentsPage )}>
+              COMPONENTS BY SIM
+            </button>
+          </div>
+        </div>
+        <div id='sub-page'>
+          {this.state.currentPage}
+        </div>
       </div>
     );
   }
