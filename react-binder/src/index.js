8bfd4525fcd55247fa0ac4008068a9774c249c16
chrisklus <christopher.klusendorf@colorado.edu>
2019-07-22 18:02:46 -0600

    Rename directory structure, see https://github.com/phetsims/binder/issues/26

--- react-binder/src/index.js
+++ react-binder/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import IndexPage from './pages/indexPage/IndexPage';
+import IndexPage from './pages/index/index';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<IndexPage />, document.getElementById('root'));
