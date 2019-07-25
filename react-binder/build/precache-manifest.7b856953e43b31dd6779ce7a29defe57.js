53f20fa1cd3b75d9097b062536aafae2ba2a87a4
chrisklus <christopher.klusendorf@colorado.edu>
2019-07-25 01:03:24 -0600

    Build and deploy latest version, see https://github.com/phetsims/binder/issues/26

---
+++ react-binder/build/precache-manifest.7b856953e43b31dd6779ce7a29defe57.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "685d0d4d9226f41f122b31e413658644",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6875491719a035a4a98d",
+    "url": "./static/css/main.040eca56.chunk.css"
+  },
+  {
+    "revision": "9634a2804d5597ac62b3",
+    "url": "./static/js/2.548e0644.chunk.js"
+  },
+  {
+    "revision": "6875491719a035a4a98d",
+    "url": "./static/js/main.e363103d.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
