dadbc66a2ed6dad9a284e19a13fbca162c698a53
Sam Reid <reids@colorado.edu>
2024-09-25 20:38:37 -0600

    Upgrade to ESLint 9 and flat config, see https://github.com/phetsims/chipper/issues/1474

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,4 @@
 // Copyright 2018, University of Colorado Boulder
 
-'use strict';
-
-/* eslint-env node */
-
 // use chipper's gruntfile
 module.exports = require( '../chipper/js/grunt/Gruntfile.js' );
