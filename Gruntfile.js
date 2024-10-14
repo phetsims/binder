2bd64bb586bfada59d4ba5245a71c2d8408d21cd
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-10-14 15:54:22 -0600

    import top level chipper grunt file, https://github.com/phetsims/chipper/issues/1464

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,4 @@
 // Copyright 2018, University of Colorado Boulder
 
 // use chipper's gruntfile
-module.exports = require( '../chipper/js/grunt/Gruntfile.js' );
+module.exports = require( '../chipper/Gruntfile' );
